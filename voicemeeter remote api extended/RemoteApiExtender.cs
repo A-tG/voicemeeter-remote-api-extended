0e16989f1015801d19f15d325d10b64835773368
A-tG <a3tg.developer@gmail.com>
2021-02-03 09:55:30 +0300

    documentation comment for GetLevel

--- voicemeeter remote api extended/RemoteApiExtender.cs
+++ voicemeeter remote api extended/RemoteApiExtender.cs
@@ -27,6 +27,20 @@ namespace AtgDev.Voicemeeter
             return resp;
         }
 
+        /// <summary>
+        ///     <para>Get Current levels.</para>
+        ///     this function must be called from one thread only
+        /// </summary>
+        /// <param name="type">What type of level to read</param>
+        /// <param name="channel">Audio channel</param>
+        /// <param name="val">The variable receiving the wanted value.</param>
+        /// <returns>
+        ///     0: OK (no error).<br/>
+        ///     -1: error<br/>
+        ///     -2: no server.<br/>
+        ///     -3: no level available<br/>
+        ///     -4: out of range<br/>
+        /// </returns>
         public Int32 GetLevel(VoicemeeterLevelType type, VoicemeeterChannel channel, out Single val)
         {
             return GetLevel((int)type, (int)channel, out val);
