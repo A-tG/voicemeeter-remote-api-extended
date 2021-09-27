af2d074422bc2184a730da3604599ff1f8b8a809
A-tG <a3tg.developer@gmail.com>
2021-09-27 21:40:18 +0300

    added async WaitForNewParams* methods,

--- voicemeeter remote api extended/RemoteApiExtender partial/RemoteApiExtender.cs
+++ voicemeeter remote api extended/RemoteApiExtender partial/RemoteApiExtender.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using AtgDev.Voicemeeter.Types;
 
 namespace AtgDev.Voicemeeter
@@ -21,15 +23,38 @@ namespace AtgDev.Voicemeeter
         /// <returns>
         ///     Same as IsParametersDirty()
         /// </returns>
-        public Int32 WaitForNewParams(int timeout = 1000, int tickTime = 1000 / 60)
+        public Int32 WaitForNewParams(int maxTime = 1000, int tickTime = 1000 / 60)
         {
-            var resp = IsParametersDirty();
-            for (int time = 0; time < timeout; time += tickTime)
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Int32 resp;
+            while (((resp = IsParametersDirty()) == 0) && (stopwatch.ElapsedMilliseconds <= maxTime))
             {
-                if (resp != 0) break;
                 System.Threading.Thread.Sleep(tickTime);
-                resp = IsParametersDirty();
-            }
+            };
+            return resp;
+        }
+
+        /// <summary>
+        ///     Waiting for voicemeeter's parameters to update, exits if waiting too long<br/>
+        ///     Crucial to call this method after the Login()<br/>
+        ///     Calls internaly IsParametersDirty() in a loop
+        /// </summary>
+        /// <param name="maxTime">Max time in ms before function exit (ms)</param>
+        /// <param name="tickTime">Time in ms between requests (ms)</param>
+        /// <returns>
+        ///     Same as IsParametersDirty()
+        /// </returns>
+        public async Task<Int32> WaitForNewParamsAsync(double maxTime = 1000, double tickTime = 1000 / 60)
+        {
+            var timeSpan = TimeSpan.FromMilliseconds(tickTime);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Int32 resp;
+            while (((resp = IsParametersDirty()) == 0) && (stopwatch.ElapsedMilliseconds <= maxTime))
+            {
+                await Task.Delay(timeSpan);
+            };
             return resp;
         }
 
@@ -41,17 +66,40 @@ namespace AtgDev.Voicemeeter
         /// <param name="maxTime">Max time in ms before function exit (ms)</param>
         /// <param name="tickTime">Time in ms between requests (ms)</param>
         /// <returns>
-        ///     Same as IsParametersDirty()
+        ///     Same as MacroButtonIsDirty()
         /// </returns>
-        public Int32 WaitForMacroNewParams(int timeout = 1000, int tickTime = 1000 / 60)
+        public Int32 WaitForMacroNewParams(int maxTime = 1000, int tickTime = 1000 / 60)
         {
-            int resp = MacroButtonIsDirty();
-            for (int time = 0; time < timeout; time += tickTime)
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Int32 resp;
+            while (((resp = MacroButtonIsDirty()) == 0) && (stopwatch.ElapsedMilliseconds <= maxTime))
             {
-                if (resp != 0) break;
                 System.Threading.Thread.Sleep(tickTime);
-                resp = MacroButtonIsDirty();
-            }
+            };
+            return resp;
+        }
+
+        /// <summary>
+        ///     Waiting for voicemeeter's macro buttons parameters to update, exits if waiting too long<br/>
+        ///     Might be crucial to call this method after the Login() in order to be able to get correct macro buttons status<br/>
+        ///     Calls internaly MacroButtonIsDirty() in a loop
+        /// </summary>
+        /// <param name="maxTime">Max time in ms before function exit (ms)</param>
+        /// <param name="tickTime">Time in ms between requests (ms)</param>
+        /// <returns>
+        ///     Same as MacroButtonIsDirty()
+        /// </returns>
+        public async Task<Int32> WaitForMacroNewParamsAsync(int maxTime = 1000, int tickTime = 1000 / 60)
+        {
+            var timeSpan = TimeSpan.FromMilliseconds(tickTime);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Int32 resp;
+            while (((resp = MacroButtonIsDirty()) == 0) && (stopwatch.ElapsedMilliseconds <= maxTime))
+            {
+                await Task.Delay(timeSpan);
+            };
             return resp;
         }
 
