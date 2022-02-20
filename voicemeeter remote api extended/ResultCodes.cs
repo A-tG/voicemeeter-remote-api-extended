cf64137040bc03fe5dfcef61422a73dbeb5676d5
A-tG <a3tg.developer@gmail.com>
2022-02-20 05:48:59 +0300

    added ProcNotImported in ResultCodes

--- voicemeeter remote api extended/ResultCodes.cs
+++ voicemeeter remote api extended/ResultCodes.cs
@@ -2,6 +2,7 @@
 {
     public static class ResultCodes
     {
+        public const int ProcNotImported = RemoteApiWrapper.ProcedureNotImportedErrorCode;
         public const int Ok = 0;
         public const int Error = -1;
         public const int NoServer = -2;
